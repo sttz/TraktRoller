6c838592e9f3aa6f3c3fb3152597c3cf592bf17b
Adrian Stutz <adrian@sttz.ch>
2018-11-25 19:09:54 +0100

    parcel-plugin-header: Use parcel's resolve to find package.json, move it out of packager's start

--- parcel-plugin-header/index.js
+++ parcel-plugin-header/index.js
@@ -1,48 +1,55 @@
 const path = require('path');
-const json5 = require('json5');
 const fs = require('fs');
+const config = require.main.require("../src/utils/config");
 
 function lineCounter(string) {
-    let lines = 1;
-    for (let i = 0; i < string.length; i++) {
-        if (string.charAt(i) === '\n') {
-        lines++;
-        }
+  let lines = 1;
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) === '\n') {
+      lines++;
     }
-    return lines;
+  }
+  return lines;
 }
 
-module.exports = function(bundler) {
-    let jsPackager = bundler.packagers.get('js');
-    if (!jsPackager) {
-        console.error("parcel-plugin-header: Could not get parcel's base JS loader.");
-        return;
+module.exports = async function(bundler) {
+  let jsPackager = bundler.packagers.get('js');
+  if (!jsPackager) {
+    console.error("parcel-plugin-header: Could not get parcel's base JS loader.");
+    return;
+  }
+
+  let relative = path.join(bundler.options.rootDir, 'index');
+  let pkg = await config.load(relative, ['package.json']);
+  if (!pkg) {
+    console.error("parcel-plugin-header: Could not find package.json.");
+    return;
+  }
+
+  let header;
+  let headerField = pkg.header;
+  if (headerField) {
+    if (fs.existsSync(headerField)) {
+      header = fs.readFileSync(headerField).toString();
+    } else {
+      header = headerField;
     }
+  }
 
-    class HeaderJSPackager extends jsPackager {
-        async start() {
-            let cwd = process.cwd();
+  if (!header) {
+    console.error("parcel-plugin-header: Header field not set in package.json.");
+    return;
+  }
 
-            let header = "";
-            let pkgPath = path.join(cwd, "package.json");
-            if (fs.existsSync(pkgPath)) {
-                let pkgData = fs.readFileSync(pkgPath).toString();
-                let pkg = json5.parse(pkgData);
-                let headerField = pkg.header;
-                if (headerField) {
-                    if (fs.existsSync(headerField)) {
-                        header = fs.readFileSync(headerField).toString();
-                    } else {
-                        header = headerField;
-                    }
-                }
-            }
-
-            await this.dest.write(header);
-            await super.start();
-            this.lineOffset += lineCounter(header);
-        }
+  let headerLines = lineCounter(header);
+  
+  class HeaderJSPackager extends jsPackager {
+    async start() {
+      await this.dest.write(header);
+      await super.start();
+      this.lineOffset += headerLines;
     }
-
-    bundler.packagers.add('js', HeaderJSPackager);
+  }
+  
+  bundler.packagers.add('js', HeaderJSPackager);
 }
